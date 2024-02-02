c9051e8aa544db44ad7643fe3eea2ecc6b12e028
Twekky <76817719+Twekky@users.noreply.github.com>
2024-02-02 09:33:38 +0000

    Hash Update

--- Modules/internalTestingModule.lua
+++ Modules/internalTestingModule.lua
@@ -1,4 +1,17 @@
--- Hash: rgmSZJgDRewaTQQLMCaTwcRXZyUFDMst
+-- Hash: bYgAwQWhswzLgiXVuqJXZGNcDJpmdEWW
+
+--
+-- ██╗  ██╗██╗   ██╗ ██████╗ ███████╗     ██████╗  █████╗ ███╗   ███╗███████╗███████╗
+-- ██║  ██║██║   ██║██╔════╝ ██╔════╝    ██╔════╝ ██╔══██╗████╗ ████║██╔════╝██╔════╝
+-- ███████║██║   ██║██║  ███╗█████╗      ██║  ███╗███████║██╔████╔██║█████╗  ███████╗
+-- ██╔══██║██║   ██║██║   ██║██╔══╝      ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  ╚════██║
+-- ██║  ██║╚██████╔╝╚██████╔╝███████╗    ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗███████║
+-- ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝ © 2024
+
+-- The following code was written exclusively for the usage within HugeGames scripts
+-- Any attempts to tamper with, modify, or unpermitted usage may result in blacklist
+-- from all HugeGames services. user data may also be recorded for securty purposes.
+--
 
 local Module = {}
 
